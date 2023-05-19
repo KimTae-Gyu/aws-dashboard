5980d5c0124b57e4909c645d21c1979e93d73d7b
kjw <job.jiwon.2022@gmail.com>
2023-05-19 14:12:04 +0900

    [v0.0.6] 지도 추가

--- d2_main.js
+++ d2_main.js
@@ -1,28 +1,60 @@
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+const fitstValue = [50,60,70,80,90,100,110,120]; //ALL영역에서 차단 IP개수를 넣기
+const firstLabel = [7,8,8,9,9,9,10,11]; //차단된 IP이름을 넣고
+
+const secondValue = [50,60,70,80,90,100,110,120];
+const secondLabel = [7,8,8,9,9,9,10,11];
 
 const firstlog = new Chart("firstlog",{
-    type: "pie",
+    type: "doughnut",
     data: {
-        labels: xValues,
-        datasets:[{
-            backgroundColor:"rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            fill: false,
-            data: yValues
-        }]
+        labels: firstLabel,
+          datasets: [{
+            label: 'My First Dataset',
+            data: fitstValue,
+            backgroundColor: [
+                'rgba(255, 67, 67, 1)',
+                'rgba(209, 51, 51, 1)',
+                'rgba(0, 114, 198, 1)',
+                'rgba(0, 182, 228, 1)',
+                'rgba(0, 163, 0, 1)',
+                'rgba(89, 197, 164, 1)',
+                'rgba(218, 247, 235, 1)',
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+          }]
+        }
     }
-});
+);
 
 const secondlog = new Chart("secondlog",{
-    type: "pie",
+    type: "doughnut",
     data: {
-        labels: xValues,
-        datasets:[{
-            backgroundColor:"rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            fill: false,
-            data: yValues
-        }]
+        labels: secondLabel,
+          datasets: [{
+            label: 'My First Dataset',
+            data: secondValue,
+            backgroundColor: [
+                'rgba(255, 67, 67, 1)',
+                'rgba(209, 51, 51, 1)',
+                'rgba(0, 114, 198, 1)',
+                'rgba(0, 182, 228, 1)',
+                'rgba(0, 163, 0, 1)',
+                'rgba(89, 197, 164, 1)',
+                'rgba(218, 247, 235, 1)',
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+          }]
+        }
     }
-});
+);
+
+var hanRiverPos = [37.5118, 126.9745];
+var map = L.map('map').setView(hanRiverPos, 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
